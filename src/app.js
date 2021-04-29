896f704173ce03c85432f30734406568db05cbdd
kotita49 <kotita49@gmail.com>
2021-04-29 12:27:18 +0100

    api works

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const router  = require("./router")
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static("public"))
+app.set("views", "views")
+app.set("view engine", "hbs")
+app.use("/", router)
 
-app.get("/", (req, res)=>{
-    res.sendFile("index.html", {
-        root: path.join(__dirname, "../public")
-    })
-})
+// app.get("/", (req, res)=>{
+//     res.sendFile("index.html", {
+//         root: path.join(__dirname, "../public")
+//     })
+// })
 
-app.get("/about", (req, res)=>{
-    res.sendFile("about.html", {
-        root: path.join(__dirname, "../public")
-    })
-})
+// app.get("/about", (req, res)=>{
+//     res.sendFile("about.html", {
+//         root: path.join(__dirname, "../public")
+//     })
+// })
 app.listen(3000, ()=>{
     console.log("The server is running on port 3000")
 })
---
+++ src/controllers/controller.js
@@ -0,0 +1,41 @@
+const axios = require("axios")
+const API_KEY = "da2dbfe066bcaf3d9beda6d60e733701";
+const Weather = require('../model/Weather')
+
+
+exports.renderHomePage = (req, res) =>{
+    res.render("index")
+}
+exports.getWeather=(req, res)=>{
+    
+    const city = req.body.city
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    const weather = new Weather(req.body.city)
+    weather.validateUserInput();
+    if(weather.errors.length){
+        res.render("index", {
+            error: weather.errors.toString()
+        })
+    } else{
+     axios.get(url).then((response)=>{
+         
+        //  if(response.status=='404'){
+        //     res.render("index", {
+        //         weather: 'Location not found'
+        //     })
+        //  }
+         const temp  = response.data.main.temp
+         const location = response.data.name
+        res.render("index", {
+            weather: `It is currently ${temp} degrees C in ${location}`
+        })
+       //console.log(`It is currently ${response.data.main.temp} in ${response.data.name}`)
+    }).catch((error) => {
+console.log(error)
+    })
+}
+
+}
+exports.renderAboutPage = (req, res)=>{
+    res.render("about")
+}
